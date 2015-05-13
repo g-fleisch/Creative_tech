d9599475509317061171c1ffa8d948916da80f8f
odanas <sanado@mnectar.com>
2015-05-13 11:38:30 -0700

    Added some assets and bounding box
    CHECK IT!

--- Galaxy_Zero/android/main.js
+++ Galaxy_Zero/android/main.js
@@ -1,20 +1,21 @@
 
-var game = new Phaser.Game(320, 480, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(320, 480, Phaser.AUTO, 'phaser-example', 
+    { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
-/*    game.load.crossOrigin = 'anonymous';
+//game.load.crossOrigin = 'anonymous';
     game.load.image('bullet', 'assets/bullet.png');
     game.load.image('enemyBullet', 'assets/enemy-bullet.png');
     game.load.spritesheet('invader', 'assets/invader32x32x4.png', 32, 32);
-    game.load.image('ship', 'assets/player.png');
+    game.load.image('ship', 'assets/ship.png');
     game.load.spritesheet('kaboom', 'assets/explode.png', 128, 128);
     game.load.image('starfield', 'assets/starfield.png');
     game.load.image('background', 'assets/background2.png');
-*/
+    game.load.image('hp', 'assets/hp.png')
     //game.load.spritesheet('invader', 'assets/invader32x32x4.png', 32, 32);
     //game.load.spritesheet('kaboom', 'assets/explode.png', 128, 128);
-
+/*
     var bulletLoad = new Image();
     bulletLoad.src = 'assets/bullet.png';
     game.cache.addImage('bullet', 'assets/bullet.png', bulletLoad);
@@ -25,8 +26,7 @@ function preload() {
 
     var invadeLoad = new Image();
     invadeLoad.src = 'assets/invader32x32x4.png';
-    game.cache.addSpriteSheet('invader', 'assets/invader32x32x4.png', invadeLoad, 4, 4);
-    //game.cache.addSpriteSheet('invader', 'assets/enemy-bullet.png', invadeLoad, 1, 1);
+    game.cache.addSpriteSheet('invader', 'assets/invader32x32x4.png', invadeLoad, 32, 32, 4, 0, 0);
 
     var playerLoad = new Image();
     playerLoad.src = 'assets/player.png';
@@ -34,8 +34,7 @@ function preload() {
 
     var kaboomLoad = new Image();
     kaboomLoad.src = 'assets/explode.png';
-    game.cache.addSpriteSheet('kaboom', 'assets/explode.png', kaboomLoad, 0, 0);
-    //game.cache.addSpriteSheet('kaboom', 'assets/enemy-bullet.png', kaboomLoad, 0, 0);
+    game.cache.addSpriteSheet('kaboom', 'assets/explode.png', kaboomLoad, 128, 128, 16, 0, 0);
 
     var stafieldLoad = new Image();
     stafieldLoad.src = 'assets/starfield.png';
@@ -43,8 +42,7 @@ function preload() {
 
     var bgLoad = new Image();
     bgLoad.src = 'assets/background2.png';
-    //game.cache.addImage('background2', 'assets/background2.png', bgLoad);
-
+*/
 
 }
 
@@ -64,6 +62,7 @@ var enemyBullet;
 var firingTimer = 0;
 var stateText;
 var livingEnemies = [];
+var oddEvenBullet = 1;
 
 function create() {
 
@@ -76,7 +75,7 @@ function create() {
     bullets = game.add.group();
     bullets.enableBody = true;
     bullets.physicsBodyType = Phaser.Physics.ARCADE;
-    bullets.createMultiple(30, 'bullet');
+    bullets.createMultiple(20, 'bullet');
     bullets.setAll('anchor.x', 0.5);
     bullets.setAll('anchor.y', 1);
     bullets.setAll('outOfBoundsKill', true);
@@ -86,14 +85,14 @@ function create() {
     enemyBullets = game.add.group();
     enemyBullets.enableBody = true;
     enemyBullets.physicsBodyType = Phaser.Physics.ARCADE;
-    enemyBullets.createMultiple(30, 'enemyBullet');
+    enemyBullets.createMultiple(20, 'enemyBullet');
     enemyBullets.setAll('anchor.x', 0.5);
     enemyBullets.setAll('anchor.y', 1);
     enemyBullets.setAll('outOfBoundsKill', true);
     enemyBullets.setAll('checkWorldBounds', true);
 
     //  The hero!
-    player = game.add.sprite(100, 400, 'ship');
+    player = game.add.sprite(160, 420, 'ship');
     player.anchor.setTo(0.5, 0.5);
     game.physics.enable(player, Phaser.Physics.ARCADE);
 
@@ -106,28 +105,28 @@ function create() {
 
     //  The score
     scoreString = 'Score : ';
-    scoreText = game.add.text(10, 10, scoreString + score, { font: '34px Arial', fill: '#fff' });
+    scoreText = game.add.text(10, 10, scoreString + score, { font: '18px Arial', fill: '#fff' });
 
     //  Lives
     lives = game.add.group();
-    game.add.text(game.world.width - 100, 10, 'Lives : ', { font: '34px Arial', fill: '#fff' });
+    game.add.text(game.world.width - 105, 10, 'Ship Health ', { font: '18px Arial', fill: '#fff' });
 
     //  Text
-    stateText = game.add.text(game.world.centerX,game.world.centerY,' ', { font: '84px Arial', fill: '#fff' });
+    stateText = game.add.text(game.world.centerX,game.world.centerY,' ', { font: '20px Arial', fill: '#fff' });
     stateText.anchor.setTo(0.5, 0.5);
     stateText.visible = false;
 
-    for (var i = 0; i < 3; i++) 
+    for (var i = 0; i < 15; i++) 
     {
-        var ship = lives.create(game.world.width - 100 + (30 * i), 60, 'ship');
-        ship.anchor.setTo(0.5, 0.5);
-        ship.angle = 90;
-        ship.alpha = 0.4;
+        var hp = lives.create(game.world.width - 10 - (20 * i), 10, 'hp');
+        hp.anchor.setTo(1, 1);
+        hp.angle = 0;
+        hp.alpha = 0.54;
     }
 
     //  An explosion pool
     explosions = game.add.group();
-    explosions.createMultiple(30, 'kaboom');
+    explosions.createMultiple(15, 'kaboom');
     explosions.forEach(setupInvader, this);
 
     //  And some controls to play the game with
@@ -138,9 +137,9 @@ function create() {
 
 function createAliens () {
 
-    for (var y = 0; y < 4; y++)
+    for (var y = 0; y < 3; y++)
     {
-        for (var x = 0; x < 10; x++)
+        for (var x = 0; x < 5; x++)
         {
             var alien = aliens.create(x * 48, y * 50, 'invader');
             alien.anchor.setTo(0.5, 0.5);
@@ -151,10 +150,11 @@ function createAliens () {
     }
 
     aliens.x = 50;
-    aliens.y = 2;
+    aliens.y = 50;
 
-    //  All this does is basically start the invaders moving. Notice we're moving the Group they belong to, rather than the invaders directly.
-    var tween = game.add.tween(aliens).to( { x: 200 }, 1000, Phaser.Easing.Linear.None, true, 0, 500, true);
+    //  All this does is basically start the invaders moving. Notice we're moving
+    // the Group they belong to, rather than the invaders directly.
+    var tween = game.add.tween(aliens).to( { x: 100 }, 1000, Phaser.Easing.Linear.None, true, 0, 500, true);
 
     //  When the tween loops it calls descend
     tween.onLoop.add(descend, this);
@@ -177,7 +177,7 @@ function descend() {
 function update() {
 
     //  Scroll the background
-    starfield.tilePosition.y += 2;
+    starfield.tilePosition.y += 4;
 
     if (player.alive)
     {
@@ -193,15 +193,46 @@ function update() {
             player.body.velocity.x = 200;
         }
 
+
+        if (cursors.up.isDown)
+        {
+            player.body.velocity.y = -200;
+        }
+        else if (cursors.down.isDown)
+        {
+            player.body.velocity.y = 200;
+        }
+
+        //Player bounding box in frame
+        if (player.body.x < 5)
+        {
+            player.body.x = 5;
+        }
+
+        if (player.body.x > 250)
+        {
+            player.body.x = 250;
+        }
+
+        if (player.body.y < 80)
+        {
+            player.body.y = 80;
+        }
+
+        if (player.body.y > 418)
+        {
+            player.body.y = 418;
+        }
+
         //  Firing?
-        if (fireButton.isDown)
+        if (game.time.now > 2500) //(fireButton.isDown)
         {
             fireBullet();
         }
 
         if (game.time.now > firingTimer)
         {
-            enemyFires();
+            enemyFires(); 
         }
 
         //  Run collision
@@ -212,10 +243,13 @@ function update() {
 
 function render() {
 
-    // for (var i = 0; i < aliens.length; i++)
-    // {
-    //     game.debug.body(aliens.children[i]);
-    // }
+/*    
+    for (var i = 0; i < aliens.length; i++)
+    {
+         game.debug.body(aliens.children[i]);
+    }
+    game.debug.body(player); 
+*/
 
 }
 
@@ -262,8 +296,8 @@ function enemyHitsPlayer (player,bullet) {
 
     //  And create an explosion :)
     var explosion = explosions.getFirstExists(false);
-    explosion.reset(player.body.x, player.body.y);
-    explosion.play('kaboom', 30, false, true);
+    explosion.reset(player.body.x+32, player.body.y+12);
+    explosion.play('kaboom', 45, false, true);
 
     // When the player dies
     if (lives.countLiving() < 1)
@@ -305,7 +339,7 @@ function enemyFires () {
         enemyBullet.reset(shooter.body.x, shooter.body.y);
 
         game.physics.arcade.moveToObject(enemyBullet,player,120);
-        firingTimer = game.time.now + 2000;
+        firingTimer = game.time.now + 1500;
     }
 
 }
@@ -317,14 +351,20 @@ function fireBullet () {
     {
         //  Grab the first bullet we can from the pool
         bullet = bullets.getFirstExists(false);
+        //console.log(bullet);
 
         if (bullet)
         {
+            //  Grab the first bullet we can from the pool
+            bullet = bullets.getFirstExists(false);
             //  And fire it
             bullet.reset(player.x, player.y + 8);
-            bullet.body.velocity.y = -400;
-            bulletTime = game.time.now + 200;
+            bullet.body.velocity.y = -500;
+            bulletTime = game.time.now + 140;
+           // oddEvenBullet = 1.5;
+            console.log(oddEvenBullet);
         }
+
     }
 
 }
